1529cc1091f2d2e5f06bd2d66d3a09a617642d25
Rekidiang2 <rkiese6@gmail.com>
2023-11-21 09:32:34 +0100

    added head.php file

---
+++ header.php
@@ -0,0 +1,37 @@
+<?php
+  //session_start();
+  //include_once 'includes/functions.inc.php';
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>header</title>
+    <link rel="icon" href="images/logo-esu.png">
+
+    <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/styles.css">
+    
+
+</head>
+<body>
+    <!-- header section starts  -->
+    <section class="header">
+    <a href="index.php" class="logo"><img src="images/logo.png" alt="logo" width=200>
+    <nav class="navbar">
+    <a href="index.php">Gestion des Presences</a>
+    </nav>
+    <div id="menu-btn" class="fas fa-bars"></div>
+    </section>
+
+    <!-- header section ends -->
+
+</body>
+</html>
