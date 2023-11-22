efc63c654f4ad5c6bd6137ca06c1ace327ecdf90
Rekidiang2 <rkiese6@gmail.com>
2023-11-22 11:35:34 +0100

    sub-header added in index.php

--- index.php
+++ index.php
@@ -1,12 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
 </head>
+
 <body>
+    <!--Header-->
     <?php include_once 'header.php'; ?>
-    
+
+    <!--Sub-Header-->
+    <div class="sub-header">
+        <p class="left-title">Liste de Presence</p>
+
+        <div id="rigth-title">
+            <p class="left-title">Date : <?php echo date('d-m-Y');?></p>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
 </body>
+
 </html>
