d0a8f4912fb8f729b13c7eb75336292ebc784b9f
Rekidiang2 <rkiese6@gmail.com>
2023-11-22 13:12:06 +0100

    enregistrement form added

--- enregistrement.php
+++ enregistrement.php
@@ -33,10 +33,49 @@
     </div>
 
     <section class="main">
+        <div class="container1 flex-container">
+            <div id="input">
+                <div class="heading">
+                    <h3>Ajouter Agent</h3>
+                </div>
+                <?php if(isset($_GET['error'])) : ?>
+                <div class="error"><?php echo $_GET['error']; ?></div>
+                <?php endif; ?>
+                <form method="post" action="includes/registre.inc.php">
+                    <div class="form-group">
+                        <div>
+                            <input type="text" name="nom" placeholder="Nom">
+                        </div>
+                        <div>
+                            <input type="text" name="postnom" placeholder="Postnom">
+                        </div>
+                        <div>
+                            <input type="text" name="matricule" placeholder="Matricule">
+                        </div>
+                        <div>
+                            <input type="text" name="grade" placeholder="Grade">
+                        </div>
+                        <input type="submit" name="submit" class="submit-btn" value="Enregistre"
+                            aria-label="Sizing example input">
+                    </div>
 
+
+
+
+
+                </form>
+
+
+            </div>
+
+
+
+        </div>
 
 
     </section>
+
+
 
 </body>
 
