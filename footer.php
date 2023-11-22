ed1bc9d45d9bb135b047bb226dd61f90b2a7249a
Rekidiang2 <rkiese6@gmail.com>
2023-11-22 10:50:46 +0100

    footer.php file and it css added

---
+++ footer.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+    <!-- swiper css link  -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+    <!-- footer section starts  -->
+    <section class="footer">
+        <div class="box-container">
+            <div class="box">
+                <h3>Lien Important</h3>
+                <a href="https://minesu.gouv.cd"> <i class="fas fa-angle-right" target="_blank"></i>Ministere de
+                    l'ESU</a>
+                <a href="https://rekidiang2.github.io/kiese.tech/" target="_blank"> <i class="fas fa-angle-right"></i>
+                    Kiese Tech</a>
+                <a href="php.net"> <i class="fas fa-angle-right" target="_blank"></i> PHP Home</a>
+            </div>
+
+
+            <div class="box">
+                <h3>Contact</h3>
+                <a href="#"> <i class="fas fa-phone" target="_blank"></i> +243-895-951200 </a>
+                <a href="#"> <i class="fas fa-phone" target="_blank"></i> +243-999-123456 </a>
+                <a href="#"> <i class="fas fa-envelope" target="_blank"></i> rkiese6@gmail.com </a>
+            </div>
+
+            <div class="box">
+                <h3>Suivez-Nous us</h3>
+                <a href="https://github.com/rekidiang2" target="_blank"> <i class="fab fa-github"></i> github </a>
+                <a href="https://twitter.com/ReaganKiese" target="_blank"> <i class="fab fa-twitter"></i> twitter </a>
+                <a href="https://www.linkedin.com/in/kiese-diangebeni-reagan-277478287/" target="_blank"> <i
+                        class="fab fa-linkedin"></i> linkedin </a>
+            </div>
+
+            <div class="box">
+                <h3>Processus d'Implementation</h3>
+                <a href="#"> <i class="fas fa-angle-right" target="_blank"></i>Conception</a>
+                <a href="https://www.figma.com/proto/DZWZ6yxP8Gu0SFCfwCR262/presence?type=design&node-id=1-2&t=7F0FHpcQ2QOCXjx9-0&scaling=contain&page-id=0%3A1"
+                    target="_blank"> <i class="fas fa-angle-right"></i>Prototype (Figma)</a>
+                <a href="#"> <i class="fas fa-angle-right" target="_blank"></i> Code</a>
+            </div>
+        </div>
+
+        <div class="credit"> created by <span>Kiese.Tech</span> | all rights reserved - 2023 </div>
+
+    </section>
+
+    <!-- footer section ends -->
+
+    <!-- swiper js link  -->
+    <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+    <!-- custom js file link  -->
+    <script src="js/script.js"></script>
+</body>
+
+</html>
